5196ffa6d95a41ed3c691311bcfc9bbc5aff1742
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2025-02-22 08:13:51 -0500

    Updated references to build output path and daemon instance path.

--- KiConsoleFramework/noninteractive/.crontab.headstart.bash
+++ KiConsoleFramework/noninteractive/.crontab.headstart.bash
@@ -1,6 +1,6 @@
 echo ""
 echo "${0}: Launching KiConsoleFramework as a daemon..."
-nohup /cygdrive/c/Users/KevinAndrewLipscomb/Documents/SANDBOX/vocational/kalipso-infogistics/OWNREPO/KiConsoleFramework/KiConsoleFramework/bin/KiConsoleFramework.exe &
+nohup /cygdrive/c/program-files/KalipsoInfogisticsLlc/KiConsoleFramework/KiConsoleFramework/bin/Debug/KiConsoleFramework.exe &
 echo "${0}: Done."
 echo "${0}: Make sure its entry is in your crontab so that it will run as a cron job upon @reboot."
 echo ""
