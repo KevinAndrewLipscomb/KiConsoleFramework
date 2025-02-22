3c6bf11ad8ee1b42fe895000f93c6e05a3f81edb
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2025-02-22 14:29:48 -0500

    Revised to account for actual deployment practices.

--- KiConsoleFramework/noninteractive/.crontab.headstart.bash
+++ KiConsoleFramework/noninteractive/.crontab.headstart.bash
@@ -1,6 +1,6 @@
 echo ""
 echo "${0}: Launching KiConsoleFramework as a daemon..."
-nohup /cygdrive/c/program-files/Ki/KiConsoleFramework/KiConsoleFramework/bin/Debug/KiConsoleFramework.exe &
+nohup /cygdrive/c/PROJ/KiConsoleFramework/KiConsoleFramework/bin/Debug/KiConsoleFramework.exe &
 echo "${0}: Done."
 echo "${0}: Make sure its entry is in your crontab so that it will run as a cron job upon @reboot."
 echo ""
